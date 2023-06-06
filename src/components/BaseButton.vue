b26d6950c594b6b30bb709ad48130327001c33c1
Wisnu K. Ardianto <setrasariputra@gmail.com>
2023-06-06 11:16:55 +0700

    Add Image Slider Library

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button :class="classButton">{{ label }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    variant: "",
+    label: "",
+  },
+  data() {
+    return {
+      classButton: "",
+    };
+  },
+  created() {
+    this.setVariant();
+  },
+  methods: {
+    setVariant() {
+      switch (this.variant) {
+        case "danger":
+          this.classButton = "bg-red-500 text-white px-8 py-2 rounded-3xl";
+          break;
+        case "success":
+          this.classButton = "bg-teal-500 text-white px-8 py-2 rounded-3xl";
+          break;
+          case "primary":
+          this.classButton = "bg-indigo-500 text-white px-8 py-2 rounded-3xl";
+          break;
+      }
+    },
+  },
+};
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,45 @@
 <template>
   <div>
     <h1>Dashboard</h1>
+    <vueper-slides>
+      <vueper-slide
+        v-for="(slide, i) in slides"
+        :key="i"
+        :image="slide.image"
+        :title="slide.title"
+        :content="slide.content"
+      >
+      </vueper-slide>
+    </vueper-slides>
   </div>
 </template>
 
 <script>
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
+
 export default {
   name: "Dashboard",
+  components: {
+    VueperSlides,
+    VueperSlide,
+  },
+  data() {
+    return {
+      slides: [
+        {
+          title: "Slide #1",
+          content: "Slide 1 content.",
+          image: 'https://images.tokopedia.net/img/cache/1208/NsjrJu/2023/6/5/3e76574f-abaf-4558-ab80-0b056d830b3f.jpg.webp?ect=4g'
+        },
+        {
+          title: "Slide #2",
+          content: "Slide 2 content.",
+          image: 'https://images.tokopedia.net/img/cache/1208/NsjrJu/2023/6/6/87d1b9f0-614f-4c0a-92cd-d2afc233b96c.jpg.webp?ect=4g'
+        },
+      ],
+    };
+  },
 };
 </script>
+<style scoped></style>
